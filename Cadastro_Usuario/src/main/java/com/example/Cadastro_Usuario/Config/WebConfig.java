2144cc9734ea4967feed540476c767f254ace09d
alexsant22 <alexandre.santana2201@gmail.com>
2025-04-23 16:36:00 -0300

    ConfigWeb

--- Cadastro_Usuario/src/main/java/com/example/Cadastro_Usuario/Config/WebConfig.java
+++ Cadastro_Usuario/src/main/java/com/example/Cadastro_Usuario/Config/WebConfig.java
@@ -1,8 +1,9 @@
 package com.example.Cadastro_Usuario.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
