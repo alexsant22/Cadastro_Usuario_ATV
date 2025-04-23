f05a51684da35bd890c939a49a896209a9f72fdc
alexsant22 <alexandre.santana2201@gmail.com>
2025-04-23 14:06:57 -0300

    ConfigWeb

---
+++ Cadastro_Usuario/src/main/java/com/example/Cadastro_Usuario/Config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.Cadastro_Usuario.Config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permite qualquer origem com credenciais
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")  // Permite qualquer origem usando padrões
+                .allowedMethods("*")         // Permite qualquer método HTTP (GET, POST, PUT, DELETE, etc.)
+                .allowedHeaders("*")         // Permite qualquer cabeçalho
+                .allowCredentials(true)      // Permite o envio de credenciais (cookies, cabeçalhos de autenticação, etc.)
+                .maxAge(3600);               // Define o tempo de cache do CORS
+    }
+}
