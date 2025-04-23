bfd03f5d11a1c5fbf2c40a51c425916b38696fff
alexsant22 <alexandre.santana2201@gmail.com>
2025-04-23 16:08:03 -0300

    Repository

--- Cadastro_Usuario/src/main/java/com/example/Cadastro_Usuario/Repository/UsuariosRepository.java
+++ Cadastro_Usuario/src/main/java/com/example/Cadastro_Usuario/Repository/UsuariosRepository.java
@@ -3,5 +3,12 @@ package com.example.Cadastro_Usuario.Repository;
 import com.example.Cadastro_Usuario.Entity.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+
+    public List<Usuarios> findAllByNome(String nome); // Buscar com base no 'nome'
+
+    public Optional<Usuarios> findAllByCpf(String cpf); // Buscar com base no 'cpf'
 }
